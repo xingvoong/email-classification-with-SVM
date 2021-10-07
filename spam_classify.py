fb6e47699324bd2c14e793efb8307c119fc449d6
Xing Voong <xingvoong@gmail.com>
2021-10-07 09:29:15 -0700

    spam_classify.py write spam classifier and error rate graph

---
+++ spam_classify.py
@@ -0,0 +1,48 @@
+import scipy.io
+from sklearn import svm
+from sklearn import metrics
+import random
+import matplotlib.pyplot as plt
+
+spam_remain = scipy.io.loadmat('spam_remain.mat')
+spam_vali = scipy.io.loadmat('spam_vali_set.mat')
+
+spam_remain_labels = spam_remain.get("spam_remain_labels")
+spam_remain_data = spam_remain.get("spam_remain_data")
+
+clf = svm.SVC(kernel='linear')
+all_ = len(spam_remain_labels)
+
+examples = [100, 200, 500, 1000, 2000, all_]
+error_rate = [0, 0, 0, 0, 0, 0]
+count = 0
+
+# error rate graph
+for example in examples:
+    clf.fit(spam_remain_data[0:example], spam_remain_labels[0:example].ravel())
+    # predict the models
+    spam_vali_set_data = spam_vali.get("spam_vali_set_data")
+    spam_vali_set_labels = spam_vali.get("spam_vali_set_labels")
+    spam_predict = clf.predict(spam_vali_set_data)
+    accuracy_score = metrics.accuracy_score(spam_vali_set_labels, spam_predict)
+    error_rate[count] = 1 - accuracy_score
+    count += 1
+    # Accuracy
+    print("Accuracy {}".
+          format(example),
+          metrics.accuracy_score(spam_vali_set_labels, spam_predict))
+print("Error rate", error_rate)
+
+# error rate graph
+examples = [100, 200, 500, 1000, 2000, 4138]
+error_rate = [0.21083172147001938,
+              0.19148936170212771,
+              0.17311411992263059,
+              0.17988394584139267,
+              0.17601547388781436,
+              0.17698259187620891]
+plt.suptitle("Error rate graph for spam dataset")
+plt.xlabel("error rate")
+plt.ylabel("number of examples")
+plt.plot(error_rate, examples, '.r-')
+plt.show()
